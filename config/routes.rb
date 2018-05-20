1b613b3892d902bf602e5ff105f5b4bb7254130d
AdrianManteza <admanteza@mail.com>
2018-05-19 20:39:37 -0400

    first commit with Vue

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'sites#index'
+
+  get 'sites/index'
+  get 'sites/show'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
