65e15264950503c90797b8cdcebe61231d4f3f44
AdrianManteza <admanteza@mail.com>
2019-01-23 22:27:57 -0500

    setup appointments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :appointments
   root 'sites#index'
 
   get 'sites/index'
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,14 @@ ActiveRecord::Schema.define(version: 2019_01_24_031325) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "appointments", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "firstname"
