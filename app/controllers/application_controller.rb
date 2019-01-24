2aca2a3f4099e2d50ec88a709795551ae19f5a19
AdrianManteza <admanteza@mail.com>
2019-01-23 23:20:39 -0500

    implement basic User login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= begin
+      if session[:user_id]
+        User.find(session[:user_id])
+      else
+        nil
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  resources :appointments
   root 'sites#index'
+
+  resources :appointments
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   get 'sites/index'
   get 'sites/show'
-
-  resources :users
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
