39ef0d45860d61fc9c57750f1f36be5287471f31
AdrianManteza <admanteza@mail.com>
2019-01-23 21:57:06 -0500

    setup User model

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   get 'sites/index'
   get 'sites/show'
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
