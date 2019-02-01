8c684738704594b925e47c7bb911c4508d8d59c1
AdrianManteza <admanteza@mail.com>
2019-01-31 23:03:33 -0500

    paginate

--- app/javascript/users/index.vue
+++ app/javascript/users/index.vue
@@ -1,17 +1,38 @@
 <template>
-  <b-table id="users-table" striped hover :busy.sync="isBusy" :fields="fields" :items="getUsers"></b-table>
+  <div>
+    <b-table id="users-table"
+             show-empty
+             striped
+             no-provider-paging
+             :busy.sync="isBusy"
+             :fields="fields"
+             :items="users"
+             :current-page="currentPage"
+             :per-page="perPage">
+    </b-table>
+    <b-row>
+      <b-col md="6">
+        <b-pagination :total-rows="totalRows" :per-page="perPage" v-model="currentPage"/>
+      </b-col>
+    </b-row>
+  </div>
 </template>
 
 <script>
   export default {
     data () {
       return {
+        users: this.getUsers,
         fields: [
           { key: 'id', label: 'User ID' },
           { key: 'username', label: 'Username' },
           { key: 'firstname', label: 'First Name' },
           { key: 'lastname', label: 'Last Name' }
         ],
+        currentPage: 1,
+        totalRows: 0,
+        perPage: 10,
+        pageOptions: [ 5, 10, 15 ],
         isBusy: false
       }
     },
@@ -24,6 +45,7 @@
 
         return promise.then((data) => {
           const items = data.body
+          this.totalRows = items.length
           this.isBusy = false
 
           return(items)
