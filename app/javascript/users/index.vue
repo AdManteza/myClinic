c2bece9d15c8714c64c5594e6e37c5fd2b073bf8
AdrianManteza <admanteza@mail.com>
2019-01-31 20:53:05 -0500

    fetch users from the backend

--- app/javascript/users/index.vue
+++ app/javascript/users/index.vue
@@ -1,16 +1,35 @@
 <template>
-  <div>
-    {{ users }} tyy
-  </div>
+  <b-table striped hover :busy.sync="isBusy" :fields="fields" :items="getUsers"></b-table>
 </template>
 
 <script>
   export default {
-    data() {
+    data () {
       return {
-       users: 'users4545adsss'
+        fields: [
+          { key: 'id', label: 'User ID' },
+          { key: 'username', label: 'Username' },
+          { key: 'firstname', label: 'First Name' },
+          { key: 'lastname', label: 'Last Name' }
+        ],
+        isBusy: false
+      }
+    },
+    methods: {
+      getUsers (ctx) {
+        let promise = this.$http.get('/admin/users.json')
+
+        return promise.then((data) => {
+          const items = data.body
+          this.isBusy = false
+
+          return(items)
+        }).catch(error => {
+          this.isBusy = false
+
+          return []
+        })
       }
     }
   }
-
 </script>
