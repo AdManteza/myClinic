c1129db24ed285deb9cf186dd1130f1c6cd68ec3
AdrianManteza <admanteza@mail.com>
2019-02-06 22:29:59 -0500

    setup the admin sessions controller

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,15 +1,17 @@
 class Admin::SessionsController < SessionsController
+  before_action :admin_user
+
   def create
-    admin_user = AdminUser.find_by_username(params[:username])
+    respond_to do |format|
+      format.json do
+        if admin_user && admin_user.authenticate(admin_session_params[:password])
+          session[:admin_user_id] = admin_user.id
 
-    if admin_user && admin_user.authenticate(params[:password])
-      session[:admin_user_id] = admin_user.id
-
-      redirect_to user_path(id: admin_user.id), notice: "Logged in!"
-    else
-      flash.now[:alert] = "Email or password is invalid"
-
-      render "new"
+          render json: { location: admin_users_path, username: admin_user.username }
+        else
+          render json: { status: :unprocessable_entity }
+        end
+      end
     end
   end
 
@@ -17,5 +19,18 @@ class Admin::SessionsController < SessionsController
     session[:admin_user_id] = nil
 
     redirect_to root_url, notice: "Logged out!"
+  end
+
+private
+
+  def admin_user
+    @admin_user ||= AdminUser.find_by_username(admin_session_params[:username])
+  end
+
+  def admin_session_params
+    params.fetch(:admin_user_session).permit(
+      :username,
+      :password
+    )
   end
 end
