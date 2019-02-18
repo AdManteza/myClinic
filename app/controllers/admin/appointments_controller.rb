d058b53fe8e5bbe8ecc00617cc78dea75bf64e18
AdrianManteza <admanteza@mail.com>
2019-02-17 21:38:37 -0500

    fix the appointments controller

--- app/controllers/admin/appointments_controller.rb
+++ app/controllers/admin/appointments_controller.rb
@@ -1,5 +1,5 @@
 class Admin::AppointmentsController < Admin::AdminController
-  before_action :set_appointment, only: [:show, :edit, :update, :destroy]
+  before_action :appointment, only: [:show, :edit, :update, :destroy]
 
   # GET /appointments
   # GET /appointments.json
@@ -21,24 +21,20 @@ class Admin::AppointmentsController < Admin::AdminController
   def edit
   end
 
-  # POST /appointments
-  # POST /appointments.json
   def create
-    @appointment = Appointment.new(appointment_params)
+    @appointment = current_site.appointments.build(appointment_params)
 
     respond_to do |format|
       if @appointment.save
-        format.html { redirect_to @appointment, notice: 'Appointment was successfully created.' }
-        format.json { render :show, status: :created, location: @appointment }
+        format.html do; end
+        format.json { render json: @appointment }
       else
-        format.html { render :new }
+        format.html do; end
         format.json { render json: @appointment.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /appointments/1
-  # PATCH/PUT /appointments/1.json
   def update
     respond_to do |format|
       if @appointment.update(appointment_params)
@@ -51,32 +47,25 @@ class Admin::AppointmentsController < Admin::AdminController
     end
   end
 
-  # DELETE /appointments/1
-  # DELETE /appointments/1.json
   def destroy
     @appointment.destroy
+
     respond_to do |format|
       format.html { redirect_to appointments_url, notice: 'Appointment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_appointment
-      @appointment = Appointment.find(params[:id])
-    end
+private
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def appointment_params
-      params.fetch(:appointment).permit(
-        :bulk_create,
-        :start_date,
-        :end_date,
-        :starting_time,
-        :duration,
-        :interval,
-        :per_day
-      )
-    end
+  def appointment
+    @appointment ||= current_site.appointments.where(id: params[:id]).first
+  end
+
+  def appointment_params
+    params.fetch(:appointment).permit(
+      :patient_session_id,
+      :user_id
+    )
+  end
 end
