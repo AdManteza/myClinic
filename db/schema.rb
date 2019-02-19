83a0336c5a3d4838126bbbd2330e295d19b07f95
AdrianManteza <admanteza@mail.com>
2019-02-18 22:20:48 -0500

    migration to remove dates from Site

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_012135) do
+ActiveRecord::Schema.define(version: 2019_02_19_031914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,6 @@ ActiveRecord::Schema.define(version: 2019_02_19_012135) do
   create_table "sites", force: :cascade do |t|
     t.string "name"
     t.string "domain"
-    t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "homepage", default: "homepage"
