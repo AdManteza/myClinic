d04b4826675a46f6ae14deb958b3ee7254c72324
AdrianManteza <admanteza@mail.com>
2019-02-28 23:00:59 -0500

    force ssl

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module MyClinic
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.force_ssl = true unless Rails.env == 'development'
+
     config.generators do |g|
       g.test_framework :rspec,
         :fixtures => false,
