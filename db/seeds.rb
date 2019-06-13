67d96e37cab81f46714b2e2a0a5f1fc63e9270f0
AdrianManteza <admanteza@mail.com>
2019-06-12 22:55:30 -0400

    make a seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create a Site
+current_site = Site.find_or_create_by(name: 'development') do |site|
+	site.domain = 'development'
+end
+
+# Create an AdminUser
+admin_user = AdminUser.find_or_create_by(username: 'Admin') do |admin|
+	admin.password  = 'password'
+	admin.firstname = 'first_name'
+	admin.lastname  = 'last_name'
+	admin.site_id   = current_site.id
+end
+
+# Create a Patient
+User.find_or_create_by(username: 'User1') do |user|
+	user.password  = 'password'
+	user.firstname = 'user_fname'
+	user.lastname  = 'user_lname'
+	user.site_id   = current_site.id
+end
+
+puts "AdminUserUsername: 'Admin'"
+puts "AdminUserPassword: 'password'"
